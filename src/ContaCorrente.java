2e44542c82b5eff925c68c552557f2bbd253b8e7
Anderson Siqueira <contato@andersonls.com.br>
2021-03-18 21:43:20 -0300

    Criando a Subclasse
    Criando a subclasse conta corrente.

---
+++ src/ContaCorrente.java
@@ -0,0 +1,15 @@
+public class ContaCorrente extends Conta {
+    private double chequeEspecial;
+
+    public ContaCorrente(int numero, int agencia, String banco, double saldo, double chequeEspecial) {
+        super(numero, agencia, banco, saldo);
+        this.chequeEspecial = chequeEspecial;
+    }
+
+    @Override
+    public String toString() {
+        return "ContaCorrente{" +
+                "chequeEspecial=" + chequeEspecial +
+                '}';
+    }
+}
--- src/TestaConta.java
+++ src/TestaConta.java
@@ -2,5 +2,9 @@ public class TestaConta {
     public static void main(String[] args){
         Conta c1 = new Conta(1, 1, "Banco AA", 10.00);
         System.out.print(c1);
+
+        ContaCorrente cc1 = new ContaCorrente(22, 1, "Banco AA", 100.00, 1000.00);
+        System.out.println(cc1);
+
     }
 }
